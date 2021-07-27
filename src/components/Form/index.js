a0539c4f24f6deec82da444008b38d8af6963243
baptaste <babachampbenoit@gmail.com>
2021-07-27 20:04:49 +0200

    Bonus css

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Form = ({ inputValue, onInputChange, sendMessage }) => (
-  <form onSubmit={sendMessage}>
+  <form
+    className="form"
+    onSubmit={sendMessage}
+  >
     <input
       type="text"
+      className="form__message-field"
       placeholder="Saisissez votre message..."
       value={inputValue}
       onChange={onInputChange}
     />
     <button
       type="button"
+      className="form__send-btn"
+      onClick={sendMessage}
+      title="Envoyer"
     >
-      >
+      <i className="far fa-paper-plane" />
     </button>
   </form>
 );
